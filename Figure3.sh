f14e8e28461c0be91ba69f581aa9fb5eeaed1a83
alexandre-lassagne <lassagna@oregonstate.edu>
2024-06-24 16:46:33 -0700

    Update Figure3.sh

--- Figure3.sh
+++ Figure3.sh
@@ -8,11 +8,11 @@ git clone https://github.com/hewm2008/LDBlockShow.git
 
 ### VCF diploidisation (mandatory for LDBlockShow)
 
-less GWAS_femmat1_filt.vcf | sed "s|\t0:|\t0\/0:|g" | sed 's|\t1:|\t1\/1:|g' | sed 's|\t2:|\t2\/2:|g' | sed 's|\t3:|\t3\/3:|g' | sed 's|\t.:|\t.\/.:|g' | cat > GWAS_femmat1_filt_diplo.vcf
+less SNPCalling.vcf | sed "s|\t0:|\t0\/0:|g" | sed 's|\t1:|\t1\/1:|g' | sed 's|\t2:|\t2\/2:|g' | sed 's|\t3:|\t3\/3:|g' | sed 's|\t.:|\t.\/.:|g' | cat > SNPCalling.vcf_diplo.vcf
 
 
 
 
-LDBlockShow -InVCF GWAS_male_diplo.vcf -OutPut /media/alexandre/Disque/Genomes/GWAS/GWAS_male/Figures/localpeaksca4_2 
--Cutline 6.75 -Region Scaffold_4:3617045-4128509 -SeleVar 2 -InGWAS /media/alexandre/Disque/Genomes/GWAS/GWAS_male/tab_gwas_corrected/LDB_input_MLMloc4.txt 
--InGFF /media/alexandre/Disque/Genomes/GWAS/GWAS_male/GUY11_PacBio_merge.gff3
+LDBlockShow -InVCF SNPCalling.vcf_diplo.vcf -OutPut Figure3 
+-Cutline 4.20 -Region Scaffold_10:917290-968350 -SeleVar 2 -InGWAS /media/alexandre/Disque/Genomes/GWAS/GWAS_male/tab_gwas_corrected/LDB_input_MLMloc4.txt 
+-InGFF GUY11_PacBio_merge.gff3
