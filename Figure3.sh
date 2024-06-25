c5e0ce86dea6db48f9197c7f91d83846b4aa24c6
alexandre-lassagne <lassagna@oregonstate.edu>
2024-06-25 11:42:40 -0700

    Update Figure3.sh

--- Figure3.sh
+++ Figure3.sh
@@ -10,9 +10,8 @@ git clone https://github.com/hewm2008/LDBlockShow.git
 
 less SNPCalling.vcf | sed "s|\t0:|\t0\/0:|g" | sed 's|\t1:|\t1\/1:|g' | sed 's|\t2:|\t2\/2:|g' | sed 's|\t3:|\t3\/3:|g' | sed 's|\t.:|\t.\/.:|g' | cat > SNPCalling.vcf_diplo.vcf
 
+### production of the Figure3
 
 
-
-LDBlockShow -InVCF SNPCalling.vcf_diplo.vcf -OutPut Figure3 
--Cutline 4.20 -Region Scaffold_10:917290-968350 -SeleVar 2 -InGWAS /media/alexandre/Disque/Genomes/GWAS/GWAS_male/tab_gwas_corrected/LDB_input_MLMloc4.txt 
+LDBlockShow -InVCF SNPCalling.vcf_diplo.vcf -OutPut Figure3 -Cutline 4.20 -Region Scaffold_10:917290-968350 -SeleVar 2 -InGWAS /media/alexandre/Disque/Genomes/GWAS/GWAS_male/tab_gwas_corrected/LDB_input_MLMloc4.txt 
 -InGFF GUY11_PacBio_merge.gff3
